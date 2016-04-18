9517b4f7b3edbf469abbaca74e56b85f83bca7dd
Gabriel Tong <tongyangmath@gmail.com>
2016-04-18 16:34:11 +0800

    no message

--- app/models/dayu.rb
+++ app/models/dayu.rb
@@ -4,20 +4,20 @@ class Dayu < ActiveRecord::Base
   scope :findByDayuable, -> (obj) { where(:dayuable_type => obj.class.name, :dayuable_id => obj.id) }
 
   serialize :smsParam
-
+  
   def self.createByDayuable(dayuable, config)
-        dayu = self.new
-        dayu.smsType = config['smsType']
-        dayu.smsFreeSignName = config['smsFreeSignName']
-        dayu.smsParam = config['smsParam']
-        dayu.recNum = config['recNum']
-        dayu.smsTemplateCode = config['smsTemplateCode']
-        dayu.dayuable_type = dayuable.class.name
-        dayu.dayuable_id = dayuable.id
-        dayu.appkey = 123
-        dayu.sended_at = '0000-00-00 00:00:00'
-        dayu.save
-        dayu
+    dayu = self.new
+    dayu.smsType = config['smsType']
+    dayu.smsFreeSignName = config['smsFreeSignName']
+    dayu.smsParam = config['smsParam']
+    dayu.recNum = config['recNum']
+    dayu.smsTemplateCode = config['smsTemplateCode']
+    dayu.dayuable_type = dayuable.class.name
+    dayu.dayuable_id = dayuable.id
+    dayu.appkey = 123
+    dayu.sended_at = '0000-00-00 00:00:00'
+    dayu.save
+    dayu
   end
 
   def self.table_name
@@ -51,10 +51,8 @@ class Dayu < ActiveRecord::Base
 
   private
   def run2
-    p :ininininininininin
     `
     php #{LaPath} dayu:send #{self.id}
     `
-    p :ininininininininin2
   end     
 end
