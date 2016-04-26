838debff4337f44c5e72cdc0b4ab01ebc774c081
Gabriel Tong <tongyangmath@gmail.com>
2016-04-26 09:48:21 +0800

    api 登陆 ， 可发卡卷 ， 可核销卡卷， 核销功能 ，卡卷信息

--- app/models/member.rb
+++ app/models/member.rb
@@ -13,13 +13,13 @@ class Member < ActiveRecord::Base
   validates :phone, :presence=>true, :uniqueness=>true
   validates_datetime :borded_at, :allow_nil=>true
 
-  has_many :managed_clients, :class_name=>Client, :foreign_key=>:admin_phone, :primary_key=>:phone
+  has_many :managed_clients, ->{where(:client_managers=>{:admin=>1})}, :through=>:client_managers, :source=>:client#, :foreign_key=>:admin_phone, :primary_key=>:phone
   has_many :managed_members, :class_name=>Member, :through=>:managed_clients, :source=>:members
 
   has_many :client_members
   has_many :clients, :through=>:client_members
 
-  has_many :client_managers
+  has_many :client_managers, :foreign_key=>:phone, :primary_key=>:phone
   has_many :managed_shops, :through=>:client_managers, :source=>:shop
 
   # 用户具有发卷权限的卡卷
