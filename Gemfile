ac4bbe4160c26f7c563f666d20122b8cc4102fd4
Gabriel Tong <tongyangmath@gmail.com>
2016-05-06 16:16:29 +0800

    集成we chat

--- Gemfile
+++ Gemfile
@@ -46,6 +46,7 @@ gem 'bulk_insert'
 gem 'will_paginate'
 gem 'randumb'
 gem 'whenever'
+gem 'wechat'
 
 gem 'devise'
 gem 'devise-i18n'
--- config/deploy.rb
+++ config/deploy.rb
@@ -23,26 +23,24 @@ set :repo_url, 'git@git.coding.net:gabriel/rails-sl.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', '.env')
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp', 'vendor/bundle', 'public/system', 'public/assets')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
-
 namespace :deploy do
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+  after 'deploy:published', 'restart' do
+		on roles(:web) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+					execute :touch,'tmp/restart.txt'
+      	end
+      end
     end
   end
-
 end
