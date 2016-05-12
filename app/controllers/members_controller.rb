16f37ecad0ddc7fa3e25018018021e9e7b7962e3
Gabriel Tong <tongyangmath@gmail.com>
2016-05-12 18:14:11 +0800

    更新we chat repo

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -5,6 +5,13 @@ class MembersController < ApplicationController
   wechat_responder
   
   def info
+    wechat_oauth2 'snsapi_userinfo' do |openid, info|
+      logger.info '...............'
+      logger.info openid
+      logger.info info
+      # @current_user = User.find_by(wechat_openid: openid)
+      # @articles = @current_user.articles
+    end    
   end
 
   def bind
